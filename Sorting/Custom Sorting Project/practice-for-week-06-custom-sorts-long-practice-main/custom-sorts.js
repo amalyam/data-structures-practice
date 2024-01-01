014f507fce50e460286d7716a192a837dfbcbf7e
Amalya Megerman <amalyamegerman@gmail.com>
2023-12-31 21:31:00 -0500

    first 3 solutions

--- Sorting/Custom Sorting Project/practice-for-week-06-custom-sorts-long-practice-main/custom-sorts.js
+++ Sorting/Custom Sorting Project/practice-for-week-06-custom-sorts-long-practice-main/custom-sorts.js
@@ -1,13 +1,19 @@
 function ageSort(users) {
-  // Your code here
+  return users.sort((a, b) => a.age - b.age);
 }
 
 function oddEvenSort(arr) {
-  // Your code here
+  return arr.sort((a, b) => {
+    if (a % 2 === 0 && b % 2 === 1) return 1;
+    if (a % 2 === 1 && b % 2 === 0) return -1;
+    return a - b;
+  });
 }
 
 function validAnagrams(s, t) {
-  // Your code here
+  if (s.length !== t.length) return false;
+
+  return [...s].sort().join("") === [...t].sort().join("");
 }
 
 function reverseBaseSort(arr) {
